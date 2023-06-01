7ef174adf16b66bcbfe4cc8ab21a4a48539305dd
rana-rayhan <ryanrazz00@gmail.com>
2023-06-01 21:25:07 +0300

    Updated file

--- server/index.js
+++ server/index.js
@@ -3,4 +3,4 @@ require("./Config/db");
 const app = require("./app");
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on por no ${port}!`));
