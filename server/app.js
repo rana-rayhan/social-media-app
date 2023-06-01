516a7597c9fda839c553bdb6612f53f9470268e2
rana-rayhan <ryanrazz00@gmail.com>
2023-06-01 21:40:05 +0300

    Updated file

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
-require("./config/passport"); // passport authintication file ---***
+// require("./config/passport"); // passport authintication file ---***
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const passport = require("passport");
@@ -8,6 +9,36 @@ const authRoute = require("./Routes/authRoute");
 const userRoute = require("./Routes/userRoute");
 const postRoute = require("./Routes/postRoute");
 const app = express();
+//
+
+const User = require("./Models/userModel");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+
+const opts = {};
+
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = process.env.SECRET; // SECRET key form dotenv
+
+passport.use(
+  // getting token while login & sending in header, then checking token with jwt_payload
+  new JwtStrategy(opts, async (jwt_payload, done) => {
+    try {
+      //const { id } = jwt_payload; // extract id || email from jwt_payload
+
+      const user = await User.findOne({ _id: jwt_payload.id }); // find user with token id
+      if (user) {
+        //return user as req.user
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    } catch (err) {
+      return done(err, false);
+    }
+  })
+);
+
 //
 //
 // middleware
